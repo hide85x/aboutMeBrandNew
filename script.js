62465cc1f3347261642e6ba83527c105601f2051
hide <hide85x@gmail.com>
2019-05-29 16:21:55 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+function scrol(cardId) {
+    
+    const elem= document.getElementById(cardId)
+    console.log(elem.id)
+        elem.scrollIntoView({
+            behavior:"smooth",
+            block:"start"
+           
+            
+        })
+
+}  
+$(document).ready(function(){
+    $('#about').addClass('active')
+    $('li').click(function(){
+        $(this).addClass('active').siblings().removeClass('active')       
+    })
+
+
+})
